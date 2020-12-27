a543e0c0167e50cf880043db6d246c166074bb67
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-12-27 15:48:40 -0300

    Mutations

---
+++ mutation/projeto/resolvers/Mutation.js
@@ -0,0 +1,44 @@
+const { usuarios, proximoId } = require('../data/db')
+module.exports = {
+  novoUsuario(_, {nome, email, idade}) {
+    const emailExistente = usuarios.some(u => u.email === email)
+
+    if(emailExistente) {
+      throw new Error('E-mail cadastrado')
+    }
+    const novo = {
+      id: proximoId(),
+      nome,
+      email,
+      idade,
+      perfil_id: 1,
+      status: 'ATIVO'
+    }
+    usuarios.push(novo)
+    return novo
+  },
+
+  excluirUsuario(_, {id}) {
+    const i = usuarios
+      .findIndex(u => u.id === id)
+    if(i < 0) return null
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
+  },
+
+  alterarUsuario(_, { id, nome, email, idade }){
+    const i = usuarios
+      .findIndex(u => u.id === id)
+    if(i < 0) return null
+
+    const usuario = {
+      ...usuarios[i],
+      nome,
+      email,
+      idade
+    }
+
+    usuarios.splice(i, 1, usuario)
+    return usuario
+  }
+}
